9ca7963b4867183fbf20b9cb77776adf7f19b118
michiwo <mk1104kado@gmail.com>
2021-01-21 23:26:22 +0900

    WIP: タイムトラベル機能

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -30,14 +30,6 @@ function Square(props) {
   
 
 class Board extends React.Component {
-  constructor(props) { 
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    }
-  }
-
   handleClick(i) {
     const squares = this.state.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -52,8 +44,8 @@ class Board extends React.Component {
 
   renderSquare(i) {
     return <Square
-      value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}
+      value={this.props.squares[i]}
+      onClick={() => this.props.handleClick(i)}
     />;
   }
 
@@ -91,6 +83,16 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
+
   render() {
     return (
       <div className="game">
